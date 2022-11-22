6e402d7414f8499916a308631d0acb30faa7f913
USER <USER@USER-PC>
2022-11-22 00:32:24 -0600

    Agregar archivos de proyecto.

---
+++ ERP/Controllers/AccessController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERP.Controllers
+{
+    public class AccessController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string Us, string Pass)
+        {
+            try
+            {
+                using (Models.ERPEntities db = new Models.ERPEntities())
+                {
+                    var user = (from d in db.User
+                                where d.username == Us.Trim() && d.password == Pass.Trim()
+                                select d).FirstOrDefault();
+                    if (user == null) 
+                    {
+                        ViewBag.Error = "Usuario o contraseña invalidos";
+                        return View();
+                    }
+
+                    Session["User"] = user;
+                }
+                
+                return RedirectToAction("Index", "Home");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ERP/Filters/AccessFilter.cs
@@ -0,0 +1,32 @@
+﻿using ERP.Controllers;
+using ERP.Models;
+using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ERP.Filters
+{
+    public class AccessFilter : ActionFilterAttribute
+    {
+        private User user;
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try 
+            {
+                base.OnActionExecuting(filterContext);
+                user = (User)HttpContext.Current.Session["User"];
+                if (user == null)
+                {
+                    if(filterContext.Controller is AccessController == false)
+                    {
+                        filterContext.HttpContext.Response.Redirect("/Access/Login");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                filterContext.Result = new RedirectResult("~/Access/Login");
+            }
+        }
+    }
+}
