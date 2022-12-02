91344d6f235e050f432047979725b451a96e05c4
Enriquekill <angrylocobirds@gmail.com>
2022-12-02 09:48:56 -0600

    DB Enrique

--- ERP/Models/Model1.Designer.cs
+++ ERP/Models/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\OstoaLeonardo\source\repos\ERP_MVC\ERP\Models\UserRol.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'C:\Users\Enriq\Source\Repos\ERP_MVC\ERP\Models\Model1.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
